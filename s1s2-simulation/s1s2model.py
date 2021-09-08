b9705b5f676ec94aac5438aa7b4c3bd00c7dd609
biplavs <biplavs@us.watson.ibm.com>
2021-09-08 16:23:43 -0400

    reorg repo

---
+++ s1s2-simulation/s1s2model.py
@@ -0,0 +1,17 @@
+class MC:
+    c = [1, 1] # cost of each solver
+    r = [1, 1] # reliability of a solver
+
+    
+    
+    def __init__(self):
+        print ("Class for MC")
+        
+        
+    def get_c(self):
+        return self.c
+        
+    def get_r(self):
+        return self.r
+        
+            
