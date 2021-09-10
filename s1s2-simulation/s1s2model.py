a87e071d9ca140086f1cbf1b29f42665fe6e3338
biplavs <biplavs@us.watson.ibm.com>
2021-09-10 11:18:41 -0400

    with metamodel doc

--- s1s2-simulation/s1s2model.py
+++ s1s2-simulation/s1s2model.py
@@ -1,3 +1,36 @@
+
+"""
+
+   1. System - a simulation environment
+      - Config: 1S1, 1S2, 1MC
+      - states: (actions x ....)
+      - actions:  a1, a2, a3, a4
+      - current_state
+      - current_action 
+
+   2. MC
+      - mc_value: value of action, state
+      - solver_choice: {default, s2}
+      - reliability_value[solvers / state]: prob of action being selected 
+        by solver conditioned by state
+
+   4. Solver
+      - C (solver)
+      - SolverType: S1 or S2
+      - SolverOutput
+        * Action
+        * Confidence
+   
+   5. World - // design time representation pieces
+      - State
+        * Reward
+      - Action
+        * expected_value        
+      - Reward(action, state)
+
+
+    """
+
 class MC:
     c = [1, 1] # cost of each solver
     r = [1, 1] # reliability of a solver
